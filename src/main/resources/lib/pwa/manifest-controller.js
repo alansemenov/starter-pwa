97983727d954cf6ffc51200a56c1dbd0c8351354
Alan Semenov <ase@enonic.com>
2017-06-28 12:14:56 +0200

    Implemented configurable manifest.json

--- src/main/resources/lib/pwa/manifest-controller.js
+++ src/main/resources/lib/pwa/manifest-controller.js
@@ -1,10 +1,20 @@
+// Replace placeholders in manifest.json with actual values
+// if you don't want them to be generated dynamically from site config
+
 var portalLib = require('/lib/xp/portal');
 var mustache = require('/lib/xp/mustache');
 
 exports.get = function() {
     var sitePath = portalLib.getSite()._path;
     var siteUrl = portalLib.pageUrl({path: sitePath});
+    var siteConfig = portalLib.getSiteConfig();
     var params = {
+        name: siteConfig.appName,
+        shortName: siteConfig.appShortName || siteConfig.appName,
+        description: siteConfig.appDescription,
+        themeColor: siteConfig.appThemeColor,
+        backgroundColor: siteConfig.appBgColor,
+        display: siteConfig.appDisplay,
         siteUrl: (siteUrl == '/') ? '/' : siteUrl + '/',
         iconUrl : portalLib.assetUrl({path: '/icons'})
     };
