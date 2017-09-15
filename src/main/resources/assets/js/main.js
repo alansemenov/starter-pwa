b64efe3377c445e543d4f81140ad640f7cf8b9e8
Alan Semenov <ase@enonic.com>
2017-09-15 15:19:52 +0200

    Implemented async fetch of current city via API

--- src/main/resources/assets/js/main.js
+++ src/main/resources/assets/js/main.js
@@ -1,6 +1,50 @@
-(function(){
-    require('../css/styles.less');
+require('../css/styles.less');
 
-    const logStatement = 'JavaScript bundle set up correctly';
-    console.log(`${logStatement}`);
+(function(){
+
+    const geoLocationApiUrl = 'http://ip-api.com/json';
+    const defaultCity = 'Oslo';
+
+    var app = {
+        currentCity: defaultCity
+    };
+
+    app.container = document.getElementById('pwa-container');
+
+    function showMessage(message) {
+        app.container.innerHTML = message;
+    }
+
+    function showErrorMessage(error) {
+        console.log(error);
+        showMessage(`Failed to determine current city. Showing forecast for ${defaultCity}`);
+    }
+    
+    function status(response) {
+        return response.ok ? Promise.resolve(response.json()) : Promise.reject(new Error(response.statusText));
+    }
+
+    function parseJson(json) {
+        if (json.status == "success" && json.city) {
+            app.currentCity = json.city;
+            showMessage(`Current city is ${app.currentCity}`);
+
+            return Promise.resolve();
+        }
+
+        return Promise.reject(new Error(json.message))
+    }
+
+    app.fetchCurrentLocation = async () => {
+
+        fetch(geoLocationApiUrl)
+            .then(status)
+            .then(parseJson)
+            .catch(showErrorMessage);
+
+    };
+
+    (function() {
+        app.fetchCurrentLocation();
+    })();
 })();
